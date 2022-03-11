cd1e79d13a6e67de189dafda29fed279c59abc58
Marco Barbadoro <101068417+barbadoromarco@users.noreply.github.com>
2022-03-11 19:55:32 +0100

    Create my-first-react-component.jsx

---
+++ my-first-react-component.jsx
@@ -0,0 +1,12 @@
+class MyComponent extends React.Component {
+  constructor(prop) {
+    super(prop);
+  }
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    )
+  }
+}
